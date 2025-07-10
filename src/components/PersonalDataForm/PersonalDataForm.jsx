ee83b2e009e7da689e8482101e4dd366919c61b2
Elizeu <elizeu_os@hotmail.com>
2025-07-10 00:37:22 -0300

    Add personalDataForm component

---
+++ src/components/PersonalDataForm/PersonalDataForm.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const PersonalDataForm = ({data, updateFieldHandler }) => {
+  return (
+    <>
+    <h2>
+      Informe seus dados pessoais:
+    </h2>
+
+      <label>
+        *Seu nome:
+        <input type="text" name="fullName"
+          required value={data.fullName || ''} onChange={(e) => {
+            updateFieldHandler('fullName', e.target.value);
+          }} />
+      </label>
+
+      <label>
+        *Seu telefone:
+        <input type="text" name="phone"
+          required value={data.phone || ''} onChange={(e) => {
+            updateFieldHandler('phone', e.target.value);
+          }} />
+      </label>
+
+      <label>
+        Seu e-mail:
+        <input type="email" name="email"
+          value={data.email || ''} onChange={(e) => {
+            updateFieldHandler('email', e.target.value);
+          }} />
+      </label>
+    </>
+  )
+}
+
+export default PersonalDataForm
